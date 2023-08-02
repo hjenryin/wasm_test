9a280a6e14632bd125afc4d0853ab82b4f125d7e
hjenryin <72179072+hjenryin@users.noreply.github.com>
2023-08-02 17:17:54 +0800

    linux

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # wasm_test
+
+## build
+
+### wasm
+
+```
+mkdir build_js
+cd build_js
+emcmake cmake ..
+emmake make
+```
+
+### linux
+
+```
+mkdir build_linux
+cd build_linux
+cmake ..
+make
+```
+
+## Results
+
+### linux
+
+```
+File at ../data.json
+Number of lines in file: 116362
+```
+
+### html on linux
+
+On linux, I used the _Live Server_ extension to open the html file.
+
+```
+File at data.json
+Number of lines in file: 116362
+```
--- main.cpp
+++ main.cpp
@@ -3,8 +3,19 @@
 #include <iostream>
 int main()
 {
+    // In html, File is at data.json as we preload it there in CMakeLists.txt
     auto file =
         std::ifstream("data.json", std::ifstream::binary);
+    if (!file.good())
+    {
+        // In linux / windows, if we're in the build directory, file should be in the .. folder
+        file = std::ifstream("../data.json", std::ifstream::binary);
+        std::cout << "File at ../data.json" << std::endl;
+    }
+    else
+    {
+        std::cout << "File at data.json" << std::endl;
+    }
     // get the number of lines in the file
     auto line_count = std::count(
         std::istreambuf_iterator<char>(file),
